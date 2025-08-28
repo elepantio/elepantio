7a8e26ce6dbd5a5931e48330bcc27a7dd6f4dd1c
harun-alrosyid <alrosyid07@gmail.com>
2025-08-28 09:09:16 +0700

    feat: mock api feature migration

---
+++ app/api/v1/migrate/education/route.ts
@@ -0,0 +1,145 @@
+import { NextRequest } from 'next/server';
+
+export function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const page = searchParams.get("page");
+
+  let data = {
+    responseCode: "0200",
+    data: {
+      title: "Punya e-mail, makin mudah akses apa saja!",
+      introMessage:
+        "Kalau kamu punya alamat e-mail, kamu bisa lebih gampang pakai layanan online. Kenapa?",
+      outroMessage:
+        "Jadi, kalau sudah punya e-mail, kamu bisa lebih bebas pakai berbagai aplikasi online!",
+      image:
+        "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+      list: [
+        {
+          icon: "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+          content:
+            "**E-mail itu tanda pengenalmu** di Internet karena banyak aplikasi yang butuh e-mail untuk daftar.",
+        },
+        {
+          icon: "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+          content:
+            "**E-mail buat jaga keamanan** karena biasanya dipakai untuk cek ulang (verifikasi) supaya akun kamu aman.",
+        },
+      ],
+      tips:null
+    },
+
+    responseDescription: "DONE_SUCCESS",
+  };
+  switch (page) {
+    case "2":
+      data = {
+        responseCode: "0200",
+        data: {
+          title: "Jaga passwordmu baik-baik!",
+          introMessage:
+            "Password itu ibarat kunci rumah. Kalau orang lain tahu, mereka bisa masuk ke akun kamu dan pakai seenaknya. Akibatnya, data pribadi bahkan uangmu bisa berisiko hilang. Cara mudah menjaga password:",
+          outroMessage:
+            "Ingat, password itu rahasia milikmu sendiri. Dengan jaga password, kamu ikut melindungi data dan transaksi biar tetap aman.",
+          image:
+            "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+          list: [
+            {
+              icon: "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+              content: "Jangan kasih tahu password ke siapa pun.",
+            },
+            {
+              icon: "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+              content:
+                "Jangan tulis password di tempat yang gampang dilihat orang.",
+            },
+          ],
+          tips:null
+        },
+        
+
+        responseDescription: "DONE_SUCCESS",
+      };
+
+      break;
+    case "3":
+      data = {
+        responseCode: "0200",
+        data: {
+          title: "Mau transaksi lebih mudah?\nInstall BRImo sekarang!",
+          introMessage: "Kami dengar kebutuhan kamu! Kenapa harus pakai BRImo?",
+          outroMessage:
+            "Aman dan lengkap, yuk cek kesiapan kamu untuk Install BRImo!",
+          image:
+            "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+          list: [
+            {
+              icon: "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+              content:
+                "Fiturnya lebih lengkap: Transfer uang, beli pulsa, sampai bayar tagihan, semua bisa di satu aplikasi.",
+            },
+            {
+              icon: "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+              content:
+                "Punya tabungan emas? Bisa langsung beli, jual, bahkan cetak emas melalui BRImo.",
+            },
+            {
+              icon: "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+              content:
+                "Kartu ATM hilang? Tenang, kartunya bisa diblokir lewat BRImo.",
+            },
+          ],
+          tips: {
+            title: "Tips Aman pakai BRImo",
+            list: [
+              {
+                icon: "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+                type: "info",
+                content:
+                  "**Buat Username & Password yang unik,** jangan pakai tanggal lahir, angka berurutan atau mudah ditebak. Password juga tidak boleh sama dengan Username, ya!",
+              },
+              {
+                icon: "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+                type: "error",
+                content: "`Contoh yang salah:` \nUsername, 123456, 01011998",
+              },
+              {
+                icon: "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+                type: "success",
+                content: "Contoh yang benar: \nAkhmad.Ramadhan, ARamadhan45",
+              },
+              {
+                icon: "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+                type: "info",
+                content: "**Jangan pakai tanggal lahir untuk PIN.**",
+              },
+              {
+                icon: "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+                type: "info",
+                content:
+                  "**Jangan pernah kasih tahu Username, Password, dan PIN kamu ke orang lain. **",
+              },
+              {
+                icon: "https://storage.googleapis.com/ceria-public-assets/senyum-mobile-product-info/CicilEmas-Alur-1.svg",
+                type: "info",
+                content:
+                  "**Rutin ganti Password dan PIN supaya akun lebih aman.**",
+              },
+            ],
+          },
+        },
+
+        responseDescription: "DONE_SUCCESS",
+      };
+      break;
+    default:
+      break;
+  }
+
+  return new Response(
+    JSON.stringify(data),
+    {
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+}
---
+++ app/api/v1/migrate/quiz/route.ts
@@ -0,0 +1,142 @@
+export async function GET(request: Request) {
+  // For example, fetch data from your DB here
+  const data = {
+    "responseCode": "0200",
+    "data": [
+        {
+            "question":"1. Kenapa kita harus punya e-mail?",
+            "multipleChoice":[
+                {
+                    "lable":"A. Biar keren",
+                    "value":"A"
+                },
+                {
+                    "lable":"B. Sebagai tanda pengenal di internet",
+                    "value":"B"
+                },
+                {
+                    "lable":"C. Sebagai identitas warga negara",
+                    "value":"C"
+                },
+                {
+                    "lable":"D. Sebagai alat masuk ke bank",
+                    "value":"D"
+                }
+            ]
+        },
+        {
+            "question":"2. Verifikasi akun itu buat apa ya?",
+            "multipleChoice":[
+                {
+                    "lable":"A. Memastikan keamanan informasi akun",
+                    "value":"A"
+                },
+                {
+                    "lable":"B. Membuat kamu keluar dari akun kamu",
+                    "value":"B"
+                },
+                {
+                    "lable":"C. Membuat akun kamu menjadi lemah",
+                    "value":"C"
+                },
+                {
+                    "lable":"D. Membuat akun kita dijaga oleh orang lain",
+                    "value":"D"
+                }
+            ]
+        },
+        {
+            "question":"3. Kenapa password kita harus dijaga?",
+            "multipleChoice":[
+                {
+                    "lable":"A. Untuk memberikan akses ke orang lain",
+                    "value":"A"
+                },
+                {
+                    "lable":"B. Untuk memberikan data pribadi kita ke orang lain",
+                    "value":"B"
+                },
+                {
+                    "lable":"C. Untuk mengirimkan data orang lain ke kita",
+                    "value":"C"
+                },
+                {
+                    "lable":"D. Untuk mengamankan data pribadi maupun uang dalam rekening",
+                    "value":"D"
+                }
+            ]
+        },
+        {
+            "question":"Bagaimana bentuk password yang baik dan aman?",
+            "answer":[
+                {
+                    "lable":"A. Kop!Pag1Enak",
+                    "value":"A"
+                },
+                {
+                    "lable":"B. Password",
+                    "value":"B"
+                },
+                {
+                    "lable":"C. 123456789",
+                    "value":"C"
+                },
+                {
+                    "lable":"D. 17Agustus1945",
+                    "value":"D"
+                }
+            ]
+        },
+        {
+            "question":"Hal apa yang bisa dilakukan di BRImo tapi belum ada di SenyuM?",
+            "multipleChoice":[
+                {
+                    "lable":"A. Pembukaan Tabungan Emas",
+                    "value":"A"
+                },
+                {
+                    "lable":"B. Pembukaan Rekening Tabungan",
+                    "value":"B"
+                },
+                {
+                    "lable":"C. Booking Gadai",
+                    "value":"C"
+                },
+                {
+                    "lable":"D. Transfer ke rekening lain",
+                    "value":"D"
+                }
+            ]
+        }
+    ],
+    "responseDescription": "DONE_SUCCESS"
+}
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
+ 
+export async function POST(request: Request) {
+  // Parse the request body
+  const body = await request.json();
+  const { name } = body;
+ 
+  // e.g. Insert new user into your DB
+  const newUser = {
+    "responseCode": "0200",
+    "data": {
+        "correct": 5,
+        "incorrect": 0,
+        "score":100
+    },
+    "responseDescription": "DONE_SUCCESS"
+}
+
+  
+ 
+  return new Response(JSON.stringify(newUser), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
